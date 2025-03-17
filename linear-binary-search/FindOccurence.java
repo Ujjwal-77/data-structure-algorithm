f16819142bfc10a918afe4e6dd09f0a4cf2343a5
Ujjwal-77 <ujjwalprasad19@gmail.com>
2025-03-17 20:47:25 +0530

    Data Structures

---
+++ linear-binary-search/FindOccurence.java
@@ -0,0 +1,13 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class FindOccurence {
+    public static void main(String[] args) {
+        try(FileReader fr=new FileReader("str.txt");
+        BufferedReader br=new BufferedReader(fr)){
+            int count=0;
+            
+        }
+    }
+}
